be275b2d81e231e2e178572c9c594b47a0c1983d
AShakerQA <shaker.abdul@hotmail.com>
2023-07-05 20:33:08 +0100

    AS: updated objectexamples.js

--- DevProject/jsexamples/objectexamples.js
+++ DevProject/jsexamples/objectexamples.js
@@ -3,3 +3,33 @@ let fasterShip = {
     'Fuel Type': 'Turbo Fuel',
     color: 'silver'
   }
+
+  //accessing properties of objects
+  let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  let crewCount = spaceship.numCrew;
+  let planetArray = spaceship.flightPath;
+
+  /*
+  bracket notation is used for keys that have
+  numbers, spaces or special characters in them
+  */
+  let rocketShip = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  let isActive = rocketShip['Active Mission'];
+  
+  console.log(rocketShip[propName]);
+  
