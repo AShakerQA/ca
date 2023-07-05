2d0382f04e4b1a73cf41522e2cce87a4909d7ebc
AShakerQA <shaker.abdul@hotmail.com>
2023-07-05 21:10:45 +0100

    AS: added looping in objects example

--- DevProject/jsexamples/objectexamples.js
+++ DevProject/jsexamples/objectexamples.js
@@ -63,3 +63,112 @@ const alienShip = {
 
 alienShip.retreat();
 alienShip.takeOff();
+
+//nested objects
+let bigSpaceship = {
+    passengers: [
+      {
+        name: 'K9'
+      }
+    ]
+    ,
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
+  
+  
+  let capFave = bigSpaceship.crew.captain['favorite foods'][0];
+  
+  let firstPassenger = bigSpaceship.passengers[0];
+  
+  console.log(firstPassenger);
+
+  //Pass by reference
+  let smallSpaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+  };
+  
+  const greenEnergy = obj => {
+    obj['Fuel Type'] = 'avocado oil';
+  };
+  
+  const remotelyDisable = obj => {
+    obj.disabled = true;
+  };
+  
+  greenEnergy(smallSpaceship);
+  remotelyDisable(smallSpaceship);
+  
+  console.log(smallSpaceship);
+
+
+//for...in, looping through objects
+  let loopShip = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+//for...in
+/*HINT
+for (let variableName in outerObject.innerObject) {
+    console.log(`${variableName}: ${outerObject.innerObject[variableName].propertyName}`)
+  };
+*/
+for (let crewMember in loopShip.crew){
+  console.log(`${crewMember}: ${loopShip.crew[crewMember].name}`)
+};
+//captain: Lily
+// chief officer: Dan
+// medic: Clementine
+// translator: Shauna
+
+for (let crewMember in loopShip.crew){
+  console.log(`${loopShip.crew[crewMember].name}: ${loopShip.crew[crewMember].degree}`)
+};
+//Lily: Computer Engineering
+// Dan: Aerospace Engineering
+// Clementine: Physics
+// Shauna: Conservation Science
