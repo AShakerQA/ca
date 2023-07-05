7e8edf22955a7601ec618210b82969e5d5531eff
AShakerQA <shaker.abdul@hotmail.com>
2023-07-05 11:07:43 +0100

    AS: added a func to determine who won

--- DevProject/rps/rockPaperScissors.js
+++ DevProject/rps/rockPaperScissors.js
@@ -9,6 +9,7 @@ const getUserChoice = userInput => {
     console.log('Please choose a valid option.');
   }
 };
+
 //auto generate computer choice via Math.random
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
@@ -21,3 +22,43 @@ const getComputerChoice = () => {
         return 'scissors';
     }
   };
+
+  //function to compare outcome of game
+  const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'The game is a tie!';
+    } 
+    if (userChoice === 'rock'){
+      if (computerChoice ==='paper'){
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+     if (userChoice === 'paper'){
+      if (computerChoice ==='scissors'){
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+    if (userChoice === 'scissors'){
+      if (computerChoice ==='rock'){
+        return 'The computer won!';
+      } else {
+        return 'You won!';
+      }
+    }
+  };
+
+  //interactive to user using string interpolation
+  const playGame = () => {
+    const userChoice = getUserChoice('PapER'); //input can be changed.
+    console.log(`You threw ${userChoice}`);
+    let computerChoice = getComputerChoice();
+    console.log(`Computer selected ${computerChoice}`);
+  
+    console.log(determineWinner(userChoice, computerChoice));
+  };
+  
+  playGame();
