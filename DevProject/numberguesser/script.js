044f7e94723a9e5ae359ffc63cbb1fc42337f279
AShakerQA <shaker.abdul@hotmail.com>
2023-07-05 15:02:08 +0100

    AS: refining functions

--- DevProject/numberguesser/script.js
+++ DevProject/numberguesser/script.js
@@ -2,45 +2,35 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
+const advanceRound = () => currentRoundNumber++;
 // Write your code below:
 const generateTarget = () => 
 {
     return Math.floor(Math.random() * 10);
 };
 
+const getAbsoluteDistance = (secret, guess) => Math.abs(secret - guess);
+
 const compareGuesses = (userGuess, computerGuess, secret) => {
     secret = generateTarget();
-    const userValue = Math.abs(secret - userGuess);
-    const computerValue = Math.abs(secret - computerGuess);
+    const userValue = getAbsoluteDistance(secret, userGuess);
+    const computerValue = getAbsoluteDistance(secret, computerGuess);
     // console.log('secret: ' + secret);
     // console.log('userValue: ' + userValue);
     // console.log('computerValue: ' + computerValue);
-    if (userValue === computerValue) {
-        return true;
-    } else if (userValue < computerValue) {
-        return true;
-    } else {
-        return false;
-    }
+    return userValue <= computerValue;
 };
 
 // console.log(compareGuesses(2, 9));
 
 const updateScore = winner => {
-    switch (winner){
-        case 'human':
-            humanScore++;
-            break;
-        case 'computer':
-            computerScore++;
-            break;
-        default:
-            break;
+    if (winner === 'human') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
     }
-};
+  }
 
 // console.log(updateScore('computer'));
 // console.log(humanScore);
 // console.log(computerScore);
-
-const advanceRound = () => currentRoundNumber++;
