e8cfbf79756b8c3c09705ec8d4614fe575e7cd2a
AShakerQA <shaker.abdul@hotmail.com>
2023-07-05 11:05:57 +0100

    AS: added computer choice func

--- DevProject/rps/rockPaperScissors.js
+++ DevProject/rps/rockPaperScissors.js
@@ -1,5 +1,6 @@
-console.log('hi');
+console.log('Hi');
 
+// Edge case, convert userinput into lowercase
 const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
@@ -8,3 +9,15 @@ const getUserChoice = userInput => {
     console.log('Please choose a valid option.');
   }
 };
+//auto generate computer choice via Math.random
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber){
+      case 0:
+        return 'rock';
+      case 1: 
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
