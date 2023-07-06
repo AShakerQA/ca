b79593b6a22a5504c15873c9223654098f4cdad4
AShakerQA <shaker.abdul@hotmail.com>
2023-07-06 20:01:33 +0100

    AS: added factory function example

--- DevProject/jsexamples/objectexamples.js
+++ DevProject/jsexamples/objectexamples.js
@@ -202,3 +202,28 @@ const secondRobot = {
 console.log(secondRobot.energyLevel);
 
 
+//setters
+const thirdRobot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  //check input value is a number, else throw error
+  set numOfSensors(num){
+    if(typeof num === 'number' && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0');
+    }
+  }
+};
+//set the new value using setter, notice no need to use paretheses. (similar to getters)
+thirdRobot.numOfSensors = 100;
+
+console.log(thirdRobot.numOfSensors);
