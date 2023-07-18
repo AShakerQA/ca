e6e12d197fc69acc7d3db3d57b9c2d41db930bbf
AShakerQA <shaker.abdul@hotmail.com>
2023-07-18 11:32:01 +0100

    AS: added mood iterator and formatter

--- DevProject/jsexamples/messageGenerator.js
+++ DevProject/jsexamples/messageGenerator.js
@@ -1,8 +1,34 @@
-const randommiser = num => {
+const randomiser = num => {
     return Math.floor(Math.random() * num)
 };
 
-const Mood = {
-    weather : ['sunny', 'cold', 'raining', 'windy'],
-    feeling: ['good', 'slow', 'bad', 'awesome', 'dark']
+const mood = {
+    weather : ['sunny', 'cold', 'rainy', 'windy'],
+    feeling: ['good', 'estatic', 'bad', 'awesome', 'mischievous']
 };
+
+let personalMood = [];
+
+for (const item in mood) {
+    // console.log(`${item}: ${mood[item]}`);
+    let moodIndex = randomiser(mood[item].length)
+    // console.log(moodIndex); //produces a random number for each array in mood object
+    switch(item) {
+        case 'weather':
+            personalMood.push(`It's a ${mood[item][moodIndex]} day.`)
+            break;
+        case 'feeling':
+            personalMood.push(`You are feeling ${mood[item][moodIndex]}.`);
+            break;
+        default:
+            personalMood.push('Not enough info.')
+    }
+  }
+
+  const formatMood = mood => {
+    const formatted = personalMood.join('\n')
+    console.log(formatted);
+  }
+
+//   console.log(personalMood);
+formatMood(personalMood);
