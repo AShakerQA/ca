ece1c335e3d92b409b8a1de8ccaa068f62dfc96c
AShakerQA <shaker.abdul@hotmail.com>
2023-07-18 11:12:35 +0100

    AS: added messageGenerator.js

---
+++ DevProject/jsexamples/messageGenerator.js
@@ -0,0 +1,8 @@
+const randommiser = num => {
+    return Math.floor(Math.random() * num)
+};
+
+const Mood = {
+    weather : ['sunny', 'cold', 'raining', 'windy'],
+    feeling: ['good', 'slow', 'bad', 'awesome', 'dark']
+};
